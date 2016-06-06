d6e7455b027d3c617a3bee817295438b68b60c43
vinomadhu <vinomadhu09@gmail.com>
2016-06-06 11:14:06 +0530

    Create guvi-beg-pos-neg-ze.C

---
+++ guvi-beg-pos-neg-ze.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("zero");
+	}
+	else if(n>0)
+	{
+			printf("positive");
+	}
+	else
+	{
+			printf("negative");
+	}
+	// your code goes here
+	return 0;
+}
