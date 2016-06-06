5b72f2175111c999a8b102e11e0cf8287b736c0a
vinomadhu <vinomadhu09@gmail.com>
2016-06-06 11:35:30 +0530

    Create guvi_beg_odd_or_even.c

---
+++ guvi_beg_odd_or_even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("even");
+	}
+
+	else
+	{
+			printf("odd");
+	}
+	// your code goes here
+	return 0;
+}
